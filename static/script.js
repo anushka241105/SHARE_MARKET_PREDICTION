c6c8e1d7c5904e22a325c2dd11028b3e52a785cb
Anushka <anushka241105@gmail.com>
2025-11-07 20:36:25 +0530

    frontend

--- static/script.js
+++ static/script.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
+  const form = document.querySelector(".left-panel");
+  const graph = document.querySelector(".right-panel");
+
   if (form) {
-    form.addEventListener("submit", () => {
-      alert("Generating your stock prediction chart...");
-    });
+    form.style.opacity = 0;
+    setTimeout(() => {
+      form.style.transition = "opacity 0.8s ease";
+      form.style.opacity = 1;
+    }, 200);
+  }
+
+  if (graph) {
+    graph.style.opacity = 0;
+    setTimeout(() => {
+      graph.style.transition = "opacity 0.8s ease";
+      graph.style.opacity = 1;
+    }, 400);
   }
 });
